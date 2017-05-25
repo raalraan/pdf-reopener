8b21063f9b6dc90ad09c0cc6d583c103fd3b66eb
Raymundo Ramos <raalraan@gmail.com>
2017-05-25 18:23:39 -0500

    Use sed to create shell commands that open the parsed PDF files

--- roz.zsh
+++ roz.zsh
@@ -1,5 +1,7 @@
 function roz {
 	ALL=$(ls -l /proc/*/fd 2>/dev/null)
 	PDFS=$(echo "$ALL" | grep .pdf$)
-	echo "$PDFS"
+	OCMD=$(echo "$PDFS" | sed -e 's/^.*-> /xdg-open "/g'\
+		-e 's/$/"\>\/dev\/null 2\>\/dev\/null \&/g')
+	echo "$OCMD"
 }
