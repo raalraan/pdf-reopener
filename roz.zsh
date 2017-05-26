47e81e4f377666c1be7c6f084906beff4044fc59
Raymundo Ramos <raalraan@gmail.com>
2017-05-26 01:40:24 -0500

    Variable ROZDIR created to store function data directory. All variables are local now

--- roz.zsh
+++ roz.zsh
@@ -1,29 +1,30 @@
 function roz {
+	local ROZDIR=~/.rz-sessions
 case $1 in
   save | s)
-    ALL=$(ls -l /proc/*/fd 2>/dev/null)
-    PDFS=$(echo "$ALL" | grep .pdf$)
-    OCMD=$(echo "$PDFS" | sed -e 's/^.*-> /xdg-open "/g'\
+    local ALL=$(ls -l /proc/*/fd 2>/dev/null)
+    local PDFS=$(echo "$ALL" | grep .pdf$)
+    local OCMD=$(echo "$PDFS" | sed -e 's/^.*-> /xdg-open "/g'\
       -e 's/$/"\>\/dev\/null 2\>\/dev\/null \&/g')
     if [[ $2 ]]; then
-      echo "$OCMD\nto rz-$2"
+      echo "$OCMD\nto $ROZDIR/rz-$2"
     else
-      echo "$OCMD\nto rz-default"
+      echo "$OCMD\nto $ROZDIR/rz-default"
     fi
     ;;
 
   open | o)
     if [[ $2 ]]; then
-	  if [[ $(ls ~/.rz-sessions/rz-$2 2>/dev/null) ]]; then
+	  if [[ $(ls $ROZDIR/rz-$2 2>/dev/null) ]]; then
         echo "will execute rz-$2"
 	  else
-		echo "Session \"$2\" was not found"
+		echo "session \"$2\" was not found"
 	  fi
     else
-	  if [[ $(ls ~/.rz-sessions/rz-default 2>/dev/null) ]]; then
+	  if [[ $(ls $ROZDIR/rz-default 2>/dev/null) ]]; then
         echo "will execute rz-default"
 	  else
-		echo "There is no default session saved. You can save a default session"
+		echo "there is no default session saved. you can save a default session"
 		echo "with the argument 'save' or 's' alone and then opening with the"
 		echo "argument 'open' or 'o' alone."
 	  fi
@@ -31,26 +32,26 @@ case $1 in
     ;;
 
   list | ls)
-    wc -l ~/.rz-sessions/* | sed -e '$d' -e 's/\/home\/.*\.rz-sessions\/rz-//g' -e 's/ *\([^ ]*\) *\([^ ]*\)/  \2: \1 files/g'
+    wc -l $ROZDIR/* | sed -e '$d' -e 's/ *\([^ ]*\) *\([^ ]*\)/  \2: \1 files/g' -e 's/^.*\/rz-/ /g'
 	;;
 
   move | mv)
     if [[ $2 && $3 ]]; then
-	  if [[ $(ls ~/.rz-sessions/rz-$2 2>/dev/null) ]]; then
-	    echo "will rename session $2 to $3: mv ~/.rz-sessions/rz-$2 ~/.rz-sessions/rz-$3"
+	  if [[ $(ls $ROZDIR/rz-$2 2>/dev/null) ]]; then
+	    echo "will rename session $2 to $3: mv $ROZDIR/rz-$2 $ROZDIR/rz-$3"
 	  else
-		echo "Session \"$2\" was not found"
+		echo "session \"$2\" was not found"
 	  fi
     else
-	  echo "You need to provide both the current and the new name for the session"
+	  echo "you need to provide both the current and the new name for the session"
 	  echo "       roz mv 'name' 'new name'"
     fi
 	;;
 
   remove | rm)
     if [[ $2 ]]; then
-	  if [[ $(ls ~/.rz-sessions/rz-$2 2>/dev/null) ]]; then
-	    echo "will remove session $2: rm ~/.rz-sessions/rz-$2"
+	  if [[ $(ls $ROZDIR/rz-$2 2>/dev/null) ]]; then
+	    echo "will remove session $2: rm $ROZDIR/rz-$2"
 	  else
 		echo "Session \"$2\" was not found"
 	  fi
